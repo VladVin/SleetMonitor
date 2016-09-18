3c58e28eabccf60dd5905bf04cf4990fd648b381
frolovconst <frolovconst@gmail.com>
2016-09-18 15:40:46 +0300

    taExchange branch started

---
+++ SleetMonitor/app/src/main/java/vladvin/sleetmonitor/DataExchange/DataBroker.java
@@ -0,0 +1,30 @@
+package vladvin.sleetmonitor.DataExchange;
+
+import vladvin.sleetmonitor.MapViewer;
+import vladvin.sleetmonitor.sensor_tracker.SensorData;
+
+/**
+ * Created by Carioca on 18/09/16.
+ */
+public class DataBroker {
+    //TODO: implementation
+    public void putSensorData(SensorData inputSensorData){
+
+    }
+    //TODO: determine parameters, return type and do implementation
+    public void getMapUpdates(){
+
+    }
+
+    //TODO: implementation
+    public void setMapListener(MapViewer mapViewer){
+
+    }
+
+    //TODO: implementation
+    public void stop(){
+
+    }
+
+
+}
