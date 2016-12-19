bf794e62ff6513f3ca30bc30c6493fac70e122a9
Vlad <vladt9@yandex.ru>
2016-12-19 19:38:27 +0300

    Refactor rest-api1

--- rest-api1/src/main/java/App.java
+++ rest-api1/src/main/java/App.java
@@ -9,17 +9,10 @@ public class App {
     private static LocationService locService;
 
     public static void main(String[] args) {
-//        String json = "[{\"lat\":0.325235, \"lon\":0.23425}, {\"lat\":0.325235, \"lon\":0.23425}]";
-//        new Location(3, 3), new Location(7, 7)
-
         DBManager dbm = new DBManager();
         GeoRTree geoRTree = new GeoRTree();
         locService = new LocationService(geoRTree, dbm);
         locService.start();
-
-//        String json = "[ {\"lat\" : 55.63274425319409, \"lon\" : 48.62411465495825 } , {\"lat\" : 55.8723253804882, \"lon\" : 48.871307373046875 } ]";
-//        List<Location> locs = LocationDeserializer.deserialize(json);
-//        List<Location> searchRes = locService.search(locs.get(0), locs.get(1));
 
         get("/searchLocations/:rect", (req, res) -> {
             String json = req.params(":rect").substring(5);
